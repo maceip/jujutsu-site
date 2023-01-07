5b143e4661aa4d673dda1cbb966b1146c151df11
foupp <93486795+kiratsu@users.noreply.github.com>
2023-01-06 22:36:22 -0500

    Add geist package

---
+++ packages/geist/colors.ts
@@ -0,0 +1,57 @@
+export const primary = {
+  background: '#000000',
+  foreground: '#ffffff',
+}
+
+export const accents = {
+  1: '#111',
+  2: '#333',
+  3: '#444',
+  4: '#666',
+  5: '#888',
+  6: '#999',
+  7: '#eaeaea',
+  8: '#fafafa',
+}
+
+export const error = {
+  lighter: '#f7d4d6',
+  light: '#f33',
+  default: 'red',
+  dark: '#e60000',
+}
+
+export const success = {
+  lighter: '#d3e5ff',
+  light: '#3291ff',
+  default: '#0070f3',
+  dark: '#0761d1',
+}
+
+export const warning = {
+  lighter: '#ffefcf',
+  light: '#f7b955',
+  default: '#f5a623',
+  dark: '#ab570a',
+}
+
+export const violet = {
+  lighter: '#d8ccf1',
+  light: '#8a63d2',
+  default: '#7928ca',
+  dark: '#4c2889',
+}
+
+export const cyan = {
+  lighter: '#aaffec',
+  light: '#79ffe1',
+  default: '#50e3c2',
+  dark: '#29bc9b',
+}
+
+export const highlight = {
+  purple: '#f81ce5',
+  pink: '#ff0080',
+  magenta: '#eb367f',
+  yellow: '#fff500',
+}
